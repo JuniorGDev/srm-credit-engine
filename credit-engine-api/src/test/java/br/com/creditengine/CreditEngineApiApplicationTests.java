ebf6c36029d6233a4a25b40deec05edf1a5e1daf
Junior G <juniorgeo70@hotmail.com>
2026-06-28 15:14:00 -0300

    feat: add credti-engine-api

---
+++ credit-engine-api/src/test/java/br/com/creditengine/CreditEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.creditengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditEngineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
