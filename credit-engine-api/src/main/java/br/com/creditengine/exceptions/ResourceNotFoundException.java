5d39fd3318f6751a5c01fe694f88067dfad2c883
Junior G <juniorgeo70@hotmail.com>
2026-06-29 12:19:28 -0300

    feat: create CurrencyAlreadyExistsException and ResourceNotFoundException

---
+++ credit-engine-api/src/main/java/br/com/creditengine/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.creditengine.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Long id) {
+        super(resource + " not found with id: " + id);
+    }
+}
