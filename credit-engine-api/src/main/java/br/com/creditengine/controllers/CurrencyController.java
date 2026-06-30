4964324acf428b7b4e47f17339b2ffaf2afe008a
Junior G <juniorgeo70@hotmail.com>
2026-06-30 00:25:31 -0300

    feat: add @Tag in CurrencyController

--- credit-engine-api/src/main/java/br/com/creditengine/controllers/CurrencyController.java
+++ credit-engine-api/src/main/java/br/com/creditengine/controllers/CurrencyController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "Currency",
+        description = "Endpoints for managing currencies"
+)
 @RestController
 @RequestMapping("/currencies")
 @Validated
