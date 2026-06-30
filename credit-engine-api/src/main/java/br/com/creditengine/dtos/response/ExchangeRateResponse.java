a0aad80e861d059edd15b7918182cea141f72981
Junior G <juniorgeo70@hotmail.com>
2026-06-30 19:58:39 -0300

    feat: add missing Swagger response documentation

--- credit-engine-api/src/main/java/br/com/creditengine/dtos/response/ExchangeRateResponse.java
+++ credit-engine-api/src/main/java/br/com/creditengine/dtos/response/ExchangeRateResponse.java
@@ -2,15 +2,37 @@ package br.com.creditengine.dtos.response;
 
 
 import br.com.creditengine.entities.ExchangeRate;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Schema(description = "Exchange rate response")
 public record ExchangeRateResponse(
+        @Schema(
+                description = "ID of the exchange rate",
+                example = "1"
+        )
         Long id,
+        @Schema(
+                description = "Code of the currency from",
+                example = "BRL"
+        )
         String fromCurrency,
+        @Schema(
+                description = "Code of the currency to",
+                example = "USD"
+        )
         String toCurrency,
+        @Schema(
+                description = "Rate of the exchange",
+                example = "5.00"
+        )
         BigDecimal rate,
+        @Schema(
+                description = "Date and time of the last update",
+                example = "2023-01-01T00:00:00"
+        )
         LocalDateTime updatedAt
 ) {
     public static ExchangeRateResponse from(ExchangeRate exchangeRate) {
