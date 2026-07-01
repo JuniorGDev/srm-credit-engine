ef99e95fa2040a80c421e72dc5b099871b7c51af
Junior G <juniorgeo70@hotmail.com>
2026-07-01 10:36:02 -0300

    feat: create findByFromCurrencyAndToCurrency in ExchangeRateRepository

--- credit-engine-api/src/main/java/br/com/creditengine/repositories/ExchangeRateRepository.java
+++ credit-engine-api/src/main/java/br/com/creditengine/repositories/ExchangeRateRepository.java
@@ -5,9 +5,11 @@ import br.com.creditengine.entities.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
 
-    boolean existsByFromCurrencyAndToCurrencyAndIdNot(Currency fromCurrency, Currency toCurrency, Long id);
+    Optional<ExchangeRate> findByFromCurrencyAndToCurrency(Currency fromCurrency, Currency toCurrency);
     boolean existsByFromCurrencyAndToCurrency(Currency fromCurrency, Currency toCurrency);
 }
