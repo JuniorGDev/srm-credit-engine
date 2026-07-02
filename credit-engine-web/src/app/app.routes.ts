08303f192d0ef5805659ef7f02784e7cfb3f6ec0
Junior G <juniorgeo70@hotmail.com>
2026-07-02 03:25:03 -0300

    feat: create setup project

--- credit-engine-web/src/app/app.routes.ts
+++ credit-engine-web/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Shell } from './layout/shell/shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Shell,
+    children: [
+      {
+        path: '',
+        redirectTo: 'simulation',
+        pathMatch: 'full'
+      },
+      {
+        path: 'simulation',
+        loadComponent: () =>
+          import('./pages/simulation/simulation').then((m) => m.Simulation)
+      },
+      {
+        path: 'settlements',
+        loadComponent: () =>
+          import('./pages/settlements/settlements').then((m) => m.Settlements)
+      }
+    ]
+  }
+];
